44a37c2f78c97942d6fa75d83efec19fbf7147ba
Calicolive <calicodeep@gmail.com>
2022-09-26 07:26:56 -0500

    added etch board and etch func

---
+++ script.js
@@ -0,0 +1,13 @@
+let board = document.querySelector('.board');
+board.style.gridTemplateColumns = `repeat(16, 1fr)`
+board.style.gridTemplateRows = `repeat(16, 1fr)`
+
+
+for(let i =0; i < 256; i++){
+    let square = document.createElement('div')
+    square.addEventListener('mouseover',() => {
+        square.style.backgroundColor = 'blue';
+    });
+    square.style.backgroundColor = 'white';
+    board.insertAdjacentElement('beforeend', square);
+}
